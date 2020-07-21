c704f4fbd9f87c696e11b22eee73ac85ef73a0b1
Kainan Doneson <kaindoneson@gmail.com>
2020-07-21 00:04:31 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,8 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.each_with_index { |el, index|
+    if (el == value_to_find)
+      return index
+    end
+  }
+  return nil;
 end
